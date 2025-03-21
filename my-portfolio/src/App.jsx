8b2e48418cfd2bee61a684c6da6adbf05bf25717
Oshadha Samarasinghe <124981711+OKSamarasinghe@users.noreply.github.com>
2025-03-21 14:36:04 +0530

    react vite with tailwind

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='Container bg-red-500 mx'>
+        <h1 className='text-8xl text-sky-500'>Hello tailwind with react</h1>
+        <div>
+          <h2 className='text-3xl'>this is 2xl</h2>
+        </div>
+        <div>
+          <button className='bg-amber-500 px-5 py-2 rounded-2xl hover:bg-amber-700'>Click Me!!</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
